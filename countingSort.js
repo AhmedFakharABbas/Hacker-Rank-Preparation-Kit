4e084fac5331fca52f666ad24f8c859799eba8f4
ahmedFakhar03 <75123023+AhmedFakharABbas@users.noreply.github.com>
2023-12-14 22:18:54 +0500

    updated sort .js

--- countingSort.js
+++ countingSort.js
@@ -10,5 +10,15 @@ return result
 const a = [1, 1, 3, 4, 1];
 console.log(countingSort(a))
 
+function countingSort(arr) {
+    const maxNumber = Math.max(...arr);
+    let result=Array(maxNumber+1).fill(0);
+  
+  for(const num of arr){
+      result[num]= (result[num]||0)+1;
+  }
+  return result
+  }
+
 //yet not fixed
 //its not working yet 
