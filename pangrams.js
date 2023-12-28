f59cbc32883a81d8af96adc34fca06fa7044d90d
ahmedFakhar03 <75123023+AhmedFakharABbas@users.noreply.github.com>
2023-12-29 00:05:46 +0500

    Update pangrams.js

--- pangrams.js
+++ pangrams.js
@@ -1,11 +1,17 @@
 //this function returns pangram if s is pangram 
 function pangrams(s) {
-    const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    // console.log(s)
-    const testa=s.split('')
-    console.log('testa',testa)
-    // for(const letter of s){
-    //     console.log('letter',letter)
-    // }
+    const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'W', 'x', 'y', 'z']
+    let pang=true
+    for(const word of alphabetSet){
+        if((!(s.includes(word))&&(!(s.includes(word.toUpperCase))))){
+           
+                pang=false
+                break 
+            
+           
+        }
+    }
+    return pang?'pangam':'N/a'
+
 }
-pangrams('The quick brown fox jumps over the lazy dog.')
+console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
