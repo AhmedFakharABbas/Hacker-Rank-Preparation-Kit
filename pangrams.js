3fb19d8aeedfafdd4d49762b60b660db6874c308
ahmedFakhar03 <75123023+AhmedFakharABbas@users.noreply.github.com>
2023-12-29 00:18:53 +0500

    Update pangrams.js

--- pangrams.js
+++ pangrams.js
@@ -1,17 +1,17 @@
 //this function returns pangram if s is pangram 
 function pangrams(s) {
     const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'W', 'x', 'y', 'z']
-    let pang=true
+    let pangram = true;
     for(const word of alphabetSet){
-        if((!(s.includes(word))&&(!(s.includes(word.toUpperCase))))){
+        if (!(s.includes(word)) && !(s.includes(word.toUpperCase()))) {
            
-                pang=false
+            pangram=false
                 break 
             
            
         }
     }
-    return pang?'pangam':'N/a'
+    return pangram?'pangram':'N/a'
 
 }
 console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
