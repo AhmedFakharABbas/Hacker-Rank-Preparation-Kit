3217dafd5026552f4bcbe22e73913a199048d736
ahmedFakhar03 <75123023+AhmedFakharABbas@users.noreply.github.com>
2023-12-28 23:41:21 +0500

    Update pangrams.js

--- pangrams.js
+++ pangrams.js
@@ -1,5 +1,11 @@
 //this function returns pangram if s is pangram 
 function pangrams(s) {
-    console.log(s)
+    const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    // console.log(s)
+    const testa=s.split('')
+    console.log('testa',testa)
+    // for(const letter of s){
+    //     console.log('letter',letter)
+    // }
 }
-pangrams()
+pangrams('The quick brown fox jumps over the lazy dog.')
