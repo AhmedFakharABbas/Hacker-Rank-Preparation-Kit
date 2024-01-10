177581c9aa6013ac9e4eb44e576e3556e3958071
ahmedFakhar03 <75123023+AhmedFakharABbas@users.noreply.github.com>
2024-01-10 23:07:54 +0500

    Update pangrams.js

--- pangrams.js
+++ pangrams.js
@@ -36,5 +36,85 @@ function pangrams(s) {
 console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
 //upercase comes up with ()
 
+//this function returns pangram if s is pangram 
+function pangrams(s) {
+    const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'W', 'x', 'y', 'z']
+    let pangram = true;
+    for(const word of alphabetSet){
+        if (!(s.includes(word)) && !(s.includes(word.toUpperCase()))) {
+           
+            pangram=false
+                break 
+            
+           
+        }
+    }
+    return pangram?'pangram':'N/a'
+
+}
+console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
+//upercase comes up with ()
+
+//this function returns pangram if s is pangram 
+function pangrams(s) {
+    const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'W', 'x', 'y', 'z']
+    let pangram = true;
+    for(const word of alphabetSet){
+        if (!(s.includes(word)) && !(s.includes(word.toUpperCase()))) {
+           
+            pangram=false
+                break 
+            
+           
+        }
+    }
+    return pangram?'pangram':'N/a'
+
+}
+console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
+//upercase comes up with ()
+
+
+
+//this function returns pangram if s is pangram 
+function pangrams(s) {
+    const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'W', 'x', 'y', 'z']
+    let pangram = true;
+    for(const word of alphabetSet){
+        if (!(s.includes(word)) && !(s.includes(word.toUpperCase()))) {
+           
+            pangram=false
+                break 
+            
+           
+        }
+    }
+    return pangram?'pangram':'N/a'
+
+}
+console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
+//upercase comes up with ()
+
+//this function returns pangram if s is pangram 
+function pangrams(s) {
+    const alphabetSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'W', 'x', 'y', 'z']
+    let pangram = true;
+    for(const word of alphabetSet){
+        if (!(s.includes(word)) && !(s.includes(word.toUpperCase()))) {
+           
+            pangram=false
+                break 
+            
+           
+        }
+    }
+    return pangram?'pangram':'N/a'
+
+}
+console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
+//upercase comes up with ()
+
+
+
 
 
