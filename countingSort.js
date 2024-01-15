0ab1176114714530fc2786c59e56fa3ac87807be
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2024-01-16 00:18:59 +0500

    updated counting sort

--- countingSort.js
+++ countingSort.js
@@ -30,3 +30,36 @@ function countingSort(arr) {
     }
   }
 //
+function countingSort(arr) {
+  const maxNumber = Math.max(...arr);
+  let result=Array(maxNumber+1).fill(0);
+
+for(const num of arr){
+    result[num]= (result[num]||0)+1;
+}
+return result
+}
+const a = [1, 1, 3, 4, 1];
+console.log(countingSort(a))
+
+function countingSort(arr) {
+    const maxNumber = Math.max(...arr);
+    let result=Array(maxNumber+1).fill(0);
+  
+  for(const num of arr){
+      result[num]= (result[num]||0)+1;
+  }
+  return result
+  }
+  //my code according to hackerrank that is not fixed yet
+//her is the corrected verstion according to chatgpt 
+function countingSort(arr) {
+    const frequencyArray = new Array(100).fill(0);
+
+    // Count the occurrences of each element in the input array
+    for (let i = 0; i < arr.length; i++) {
+        frequencyArray[arr[i]]++;
+    }
+  }
+//
+
