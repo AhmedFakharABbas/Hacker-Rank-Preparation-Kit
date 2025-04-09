5fff8420efae0800c87cb5e2eed6a77f5c6cd845
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2025-04-09 17:44:47 +0500

    udated the subarrayDivision1.js

--- subarrayDivision1.js
+++ subarrayDivision1.js
@@ -1,6 +1,7 @@
+// Slide through the array and calculate the sum of each subarray of length 'm'
 const birthdayfunction = function (s, d, m) {
   let result = 0;
-  for (i = 0; i <= s.length - m; i++) {
+  for (let i = 0; i <= s.length - m; i++) {
     let swindow = s.slice(i, m + i);
     if (swindow.reduce((a, b) => a + b, 0) == d) {
       result++;
