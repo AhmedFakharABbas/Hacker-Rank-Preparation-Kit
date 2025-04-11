1ff4487ddc4b4f88f9ea01c32f7ce928c0b3fc58
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2025-04-11 14:59:53 +0500

    completly solved the cahllenge

--- comparetheTriplets.js
+++ comparetheTriplets.js
@@ -1,7 +1,16 @@
 function compareTriplets(a, b) {
-  console.log(a, b);
+  let aliceScore = 0;
+  let bobScore = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      aliceScore++;
+    } else if (a[i] < b[i]) {
+      bobScore++;
+    }
+  }
+  return [aliceScore, bobScore];
 }
-compareTriplets([1, 2, 3], [3, 2, 1]); // [1, 1]
+console.log(compareTriplets([1, 2, 3], [3, 2, 1]));
 // The function should return an array with two integers, the first being the score of Alice and the second being the score of Bob.
 // The function should compare the elements of the two arrays and award points based on the following rules:
 // - If Alice's score is greater than Bob's score, Alice gets 1 point.
