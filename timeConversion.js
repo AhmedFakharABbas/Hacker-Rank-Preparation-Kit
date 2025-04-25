3e534632588d7e898a181018d17278714afaab6d
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2025-04-25 20:05:09 +0500

    time converstion function is added

---
+++ timeConversion.js
@@ -0,0 +1,29 @@
+function timeConversion(s) {
+  let jacksplit = s.split(':');
+  let hours = +jacksplit[0];
+  let minutes = +jacksplit[1];
+  let secondTime = jacksplit[2];
+
+  console.log('hours', hours);
+  console.log('houminutesrs', minutes);
+  console.log('secondTime', secondTime);
+
+  if (secondTime.includes('AM')) {
+    console.log('time is in AM');
+    // const zick = s.split(':');
+    // console.log('jack', zick);
+    hours = hours - 12;
+    if (hours == 0) {
+      hours = '00';
+    }
+    let seconds = secondTime.replace('AM', '');
+
+    console.log(`${hours}:${minutes}:${seconds}`);
+  } else {
+    console.log('time is in PM');
+    let seconds = secondTime.replace('PM', '');
+
+    console.log(`${hours}:${minutes}:${seconds}`);
+  }
+}
+timeConversion('12:01:45AM');
