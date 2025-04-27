430b79041b3e869ea99d4b20328ddf7711c8955a
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2025-04-27 08:22:17 +0500

    logs are comented

--- timeConversion.js
+++ timeConversion.js
@@ -4,10 +4,10 @@ function timeConversion(s) {
   let minutes = +jacksplit[1];
   let secondTime = jacksplit[2];
 
-  console.log('hours', hours);
-  console.log('houminutesrs', minutes);
-  console.log('secondTime', secondTime);
-  
+  // console.log('hours', hours);
+  // console.log('houminutesrs', minutes);
+  // console.log('secondTime', secondTime);
+
 
   if (secondTime.includes('AM')) {
     console.log('time is in AM');
